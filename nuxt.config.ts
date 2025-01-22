6f918ae1a0d80443f5569add5c74431294c45f1c
Ravikumar Padmanaban <ravikumar@aveoninfotech.com>
2025-01-22 21:20:27 +0530

    new user page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [['github:padmanabanravikumar/ui-mf-nuxt-core#master', { install: true }]],
   routeRules: {
-    '/users': {
+    '/users/**': {
       ssr: false
     }
   }
